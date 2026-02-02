675e0add6bef16eba5e1c1f568587294e5242321
VyLHoang <thaovyle27@gmail.com>
2026-02-02 23:20:13 +0700

    Configure allowedDevOrigins in next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    allowedDevOrigins: ["192.168.1.95", "localhost:3000"]
+  }
 };
 
 export default nextConfig;
